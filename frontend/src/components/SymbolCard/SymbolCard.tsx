d6a186463fc1bfccf633bc17e66f6ad569ce7b00
Rafael Anaya <rafael27.ar@gmail.com>
2025-03-15 15:47:37 +0100

    feat: show card info functionality implemented

--- frontend/src/components/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolCard/SymbolCard.tsx
@@ -1,10 +1,9 @@
 import './symbolCard.css';
-import { ReactComponent as CompanyIcon } from '@/assets/company.svg';
 import { useAppSelector } from '@/hooks/redux';
-import ListItem from '@/components/ListItem';
 import SymbolCardHeader from './components/SymbolCardHeader/SymbolCardHeader';
 import SymbolCardPrice from './components/SymbolCardPrice/SymbolCardPrice';
 import SymbolCardCompanyInfo from './components/SymbolCardCompanyInfo/SymbolCardCompanyInfo';
+import { selectShowCardInfo } from '@/store/dashboardOptionsSlice';
 
 type SymbolCardProps = {
   id: string;
@@ -19,18 +18,24 @@ const SymbolCard = ({ id, onClick, price }: SymbolCardProps) => {
     industry,
     marketCap
   } = useAppSelector((state) => state.stocks.entities[id]);
+  const showCardInfo = useAppSelector(selectShowCardInfo);
+
   const handleOnClick = () => {
     onClick(id);
   };
+
   return (
     <div onClick={handleOnClick} className="symbolCard">
       <SymbolCardHeader id={ id } trend={ trend } />
       <SymbolCardPrice price={ price } />
-      <SymbolCardCompanyInfo
-        companyName={companyName}
-        industry={industry}
-        marketCap={marketCap}
-      />
+      {
+        showCardInfo &&
+        <SymbolCardCompanyInfo
+          companyName={ companyName }
+          industry={ industry }
+          marketCap={ marketCap }
+        />
+      }
     </div>
   );
 };
