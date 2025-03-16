2f001fdfec4078a0d389b20b80b76ae34cccefdc
Rafael Anaya <rafael27.ar@gmail.com>
2025-03-16 11:45:26 +0100

    feat: improved bundle size with routes lazy loading

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,16 +1,28 @@
-import SymbolsView from '@/components/SymbolsView';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import StatementsView from "@/components/StatementsView";
-import ProfileView from "@/components/ProfileView";
+import { Suspense } from 'react';
+import { routes } from './routes';
+import Loading from '@/components/Loading';
 
 const Router = () => {
   return (
+    <Suspense fallback={ <Loading /> }>
       <Routes>
-        <Route index element={<SymbolsView />} />
-        <Route index path="profile" element={<ProfileView />} />
-        <Route index path="statements" element={<StatementsView />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        {
+          routes.map(({ path, component: Component }) => (
+            <Route
+              key={ path }
+              path={ path }
+              element={
+                <Suspense fallback={ <Loading /> }>
+                  <Component />
+                </Suspense>
+              }
+            />
+          ))
+        }
+        <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
+    </Suspense>
   );
 };
 
